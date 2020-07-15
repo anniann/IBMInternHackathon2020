8d7f8c7264c791be76f00b9fbdcc5ded6f7a56d9
Annie Wong <anniewong@Annies-MacBook-Pro.local>
2020-07-15 14:44:25 -0700

    incorporated landing page and uishell but no routing

--- hydrohub/src/App.js
+++ hydrohub/src/App.js
@@ -1,20 +1,90 @@
 import React from 'react';
-import './app.scss';
+import './App.scss';
 import UIShell from "./components/UIShell";
 import { Content } from 'carbon-components-react/lib/components/UIShell';
-import { Route, Switch } from 'react-router-dom';
+//import { Route, Switch } from 'react-router-dom';
 import OrgDashboard from './content/OrgDashboard'
 import './content/OrgDashboard/org-dashboard.scss'
+//import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <UIShell />
-      <Content>
-        <OrgDashboard />
-      </Content>
-    </div>
+    // <div className="App">
+    //   <UIShell />
+    //   <Content>
+    //     <OrgDashboard />
+    //   </Content>
+    // </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    );
+  }
+
+function Home() {
+  return (
+    //<div style={{ backgroundImage: `url(${require("./background2.png")})`, backgroundSize:'cover',  height: '100' }}>
+    <div style={{ backgroundImage: `url(${require("./background2.png")})`, backgroundSize:'cover',  height: '100' }}>
+      <section className="main">
+      </section>
+      
+      <section className="main">
+        <ul> 
+          <li> <img src="./images/logo.png" width="80" alt="logo" /> </li>
+          <li> <div className="main-text"> hydrohub</div> </li>
+          </ul>
+        </section>
+      <section className="main"> 
+        <ul>
+          <li> Addressing supply chain shortage<br></br> for clinics worldwide.</li>
+          
+          </ul>
+      </section>
+      <section classname="main">
+          <ul>
+            <li> <Link to="/">Dashboard</Link> </li>
+            <li> <Link to="/">Github</Link> </li>
+            </ul>
+        </section>
+      </div>
+      
   );
+    
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+function Github() {
+  return <h2>Github</h2>;
 }
 
 export default App;
---
+++ hydrohub/src/components/UIShell/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeaderContainer from "carbon-components-react/lib/components/UIShell/HeaderContainer";
+import {
+    Header,
+    HeaderMenuButton,
+    HeaderName,
+    HeaderGlobalBar,
+    HeaderGlobalAction,
+    Search,
+    SkipToContent,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    SideNavMenu,
+    SideNavMenuItem
+  } from 'carbon-components-react/lib/components/UIShell';
+
+  
+
