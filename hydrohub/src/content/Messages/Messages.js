a6a577af141fd338cc4560ef27ecd85d033cb10c
Prince Carter <princecarter@Princes-MacBook-Pro-2.local>
2020-07-16 01:04:40 -0700

    updated messages page

--- hydrohub/src/content/Messages/Messages.js
+++ hydrohub/src/content/Messages/Messages.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChatList } from "react-chat-elements";
 import UIShell from "../../components/UIShell"
+import { FormGroup, TextInput } from 'carbon-components-react'
 
 const Messages = () => {
   return (
@@ -49,16 +50,17 @@ const Messages = () => {
             date: new Date(),
             unread: 0,
           },
-          {
-            avatar: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
-            alt: "Reactjs",
-            title: "Facebook",
-            subtitle: "What are you doing?",
-            date: new Date(),
-            unread: 0,
-          },
         ]}
       />
+      <FormGroup>
+      <TextInput
+        helperText="Optional helper text."
+        id="test2"
+        invalidText="A valid value is required"
+        labelText="Text Input label"
+        placeholder="Placeholder text"
+      />
+    </FormGroup>
     </div>
   );
 };
