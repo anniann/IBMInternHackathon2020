6aea90b46f9723e4c8ea2fa3e44a46aaa2e42d2a
Annie Wong <anniewong@Annies-MacBook-Pro.local>
2020-07-16 02:21:15 -0700

    Changed Messaging MessageList

--- hydrohub/src/content/Messages/Messages.js
+++ hydrohub/src/content/Messages/Messages.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { ChatList } from "react-chat-elements";
+import { MessageList, MessageBox, ChatList } from "react-chat-elements";
 import UIShell from "../../components/UIShell"
 import { FormGroup, TextInput } from 'carbon-components-react'
+import "./messages.scss";
+import 'react-chat-elements/dist/main.css';
+
 
 const Messages = () => {
   return (
     <div>
       <UIShell />
-      <ChatList
+      {/* <ChatList
         className="chat-list"
         dataSource={[
           {
@@ -51,14 +54,69 @@ const Messages = () => {
             unread: 0,
           },
         ]}
-      />
+      /> */}
+      <MessageList
+        className='message-list'
+        lockable={true}
+        toBottomHeight={'100%'}
+        dataSource={[
+            {
+              avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              title: 'John',
+              position: 'left',
+              type: 'text',
+              text: 'Hey there! I would like to donate to your organization.',
+              date: new Date(1594800000000),
+            },
+            {
+              // avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              position: 'right',
+              type: 'text',
+              text: 'Hi, thank you so much for contacting me. We are short of many essential goods',
+              date: new Date(1594800000000),
+            },
+            {
+              // avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              position: 'right',
+              type: 'text',
+              text: 'Do you think you could help?',
+              date: new Date(1594800000000),
+            },
+            {
+              avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              title: 'John',
+              position: 'left',
+              type: 'text',
+              text: 'Yes of course, what do you need?',
+              date: new Date(1594800000000),
+            },
+            {
+              // avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              position: 'right',
+              type: 'text',
+              text: 'Water and food are pressing concerns right now.',
+              date: new Date(1594800000000),
+            },
+            {
+              avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+              title: 'John',
+              position: 'left',
+              type: 'text',
+              text: 'Alright! Expect a shipment by Wednesday!',
+              date: new Date(1594800000000),
+            
+            },
+
+      ]}
+        />
       <FormGroup>
       <TextInput
-        helperText="Optional helper text."
+        className="chat-bar"
+        //helperText="Optional helper text."
         id="test2"
         invalidText="A valid value is required"
-        labelText="Text Input label"
-        placeholder="Placeholder text"
+        //labelText="Text Input label"
+        placeholder="Type to Chat"
       />
     </FormGroup>
     </div>
